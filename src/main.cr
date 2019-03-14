380a4e0a2be1ae226d0b86cb44b288cdd2d7f1bf
kaneko <>
2019-03-14 09:46:14 +0900

    test

--- src/main.cr
+++ src/main.cr
@@ -1,14 +1,29 @@
 
 
 class HelloUnko
-    def hello
+    property name : String
+    def initialize(@name : String = "unnamed")   
+    end
+
+    def hello(word : String)
         [5, 4, 3, 2, 1].each do |num|
             puts "Count down #{num}"
         end
 
-        puts "hello, this is a pen"
+        puts "hello, this is a #{word}"
+    end
+
+    def equals? (obj : HelloUnko) : Bool
+        self.equals? obj.name
+    end
+
+    def equals? (name : String) : Bool
+        @name == name
     end
 end
 
-test = HelloUnko.new
-test.hello
+test = HelloUnko.new(name: "old")
+test.hello(test.name)
+test.name = "new name"
+test.hello(test.name)
+puts (test.equals? "new name")
